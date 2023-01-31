4cc641f73db90ae358b6bc25c250703e9a20f47f
Blake Bordelon <blakebordelon@dhcp-10-250-223-137.harvard.edu>
2023-01-31 10:02:08 -0500

    adding setup.py for pip, fixing imports

--- NTK_gd_comparison.py
+++ NTK_gd_comparison.py
@@ -2,7 +2,8 @@ import jax
 import jax.numpy as np
 from jax import random
 from jax.experimental import optimizers
-from jax.api import jit, grad, vmap
+#from jax.api import jit, grad, vmap
+from jax import jit, grad, vmap
 import functools
 import neural_tangents as nt
 from neural_tangents import stax
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NTK_Learning_Curves',
+    license = 'MIT License',
+    url='https://github.com/Pehlevan-Group/NTK_Learning_Curves',
+    author='Pehlevan Group',
+    author_email='blake_bordelon@g.harvard.edu',
+    packages = find_packages()
+)
